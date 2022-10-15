0fff2fc5056b2e3f8fe11cbfe61558866dd0f34f
AgweBryan <agwebryanmunakwa@gmail.com>
2022-10-16 00:21:08 +0100

    implement add message to firebase

--- lib/controllers/conversation_controller.dart
+++ lib/controllers/conversation_controller.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_messaging_app/models/message.dart';
+import 'package:flutter_messaging_app/utils/firebase.dart';
 import 'package:get/get.dart';
 
 class ConversationController extends GetxController {
@@ -5,5 +7,23 @@ class ConversationController extends GetxController {
 
   updateIsTyping(String text) {
     isTyping.value = text.isNotEmpty ? true : false;
+  }
+
+  addMessageToDb(Message message) async {
+    final Map<String, dynamic> map = message.toMap();
+
+    final String collectionName = 'messages';
+
+    await firestore
+        .collection(collectionName)
+        .doc(message.senderId)
+        .collection(message.receiverId)
+        .add(map);
+
+    await firestore
+        .collection(collectionName)
+        .doc(message.receiverId)
+        .collection(message.senderId)
+        .add(map);
   }
 }
